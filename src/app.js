7f16298e0a945435ab6ae32ec84b70ad3d420e8c
Asad Nouman <asadafridi30880@gmail.com>
2025-11-01 13:29:53 +0500

    Refactor routes to use app.use for endpoints

--- src/app.js
+++ src/app.js
@@ -2,18 +2,17 @@ const express = require('express');
 
 const app = express();
 
-app.get("/",(req , res) => {
-  res.send('Welcome to Node  DEVthinder')
-});
-
-app.get("/hello", (req , res) => {
+app.use("/hello", (req , res) => {
   res.send("All tests are shown from the server");
 });
 
-app.get("/test", (req, res) => {
+app.use("/test", (req, res) => {
   res.send("This is all is frontend related data");
 });
 
+app.use("/",(req , res) => {
+  res.send('Welcome to Node  DEVthinder')
+});
 
 app.listen(7777 , () => {
   console.log('Server is running on port 7777...');
